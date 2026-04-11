c6a5e351be00aae4c71f8494a7bd64581bb4467b
Owen Kellie-Smith <oks.modelling@gmail.com>
2026-04-11 16:50:08 +0100

    make nullable

--- src/App/Config.cs
+++ src/App/Config.cs
@@ -2,17 +2,19 @@ namespace PensionModel.App
 {
     public class Config
     {
-        public string MpFile { get; set; } = null;
-        public string AssetsFolder { get; set; } = "assets/xls";
-        public string MortFile { get; set; } = "pma92.xls";
+        public double Rate { get; set; } = 0.03;
+        public string AssetsFolder { get; set; } = "assets/xls"; // used to locate Mortality tables
+
+        public string? MpFile { get; set; } = null; // MpFile contains per-row definitions of MortFile, Age, Benefit
+        public double Age { get; set; } = 65; // so Age is meaningless is there is a MpFile
+        public double Benefit { get; set; } = 10000; // and similarly Benefit is meaningless if there is a MpFile
+        public string MortFile { get; set; } = "pma92.xls"; // and similarly MortFile is meaningless if there is an MpFile
+
+        public int Years { get; set; } = 10;
+        public string Agg { get; set; } = "sum_year";
+
         public string Output { get; set; } = "out.csv";
 
-        public double Age { get; set; } = 65;
-        public double Benefit { get; set; } = 10000;
-        public double Rate { get; set; } = 0.03;
-        public int Years { get; set; } = 10;
-
-        public string Agg { get; set; } = "sum_year";
         public bool Debug { get; set; } = false;
         public bool ShowHelp { get; set; } = false;
     }
