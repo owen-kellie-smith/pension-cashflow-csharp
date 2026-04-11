80d71f4dc202fe1b290af972a6d8b86a81a6b126
Owen Kellie-Smith <oks.modelling@gmail.com>
2026-04-11 17:30:28 +0100

    Comment ArgsParser

--- src/App/ArgsParser.cs
+++ src/App/ArgsParser.cs
@@ -8,53 +8,42 @@ namespace PensionModel.App
         public static Config Parse(string[] args)
         {
             var config = new Config();  // from App.Config.cs which is POCO with default values
-            var dict = DictArgs();
-
-// Source - https://stackoverflow.com/a/16265268
-// Posted by Hossein Narimani Rad, modified by community. See post 'Timeline' for change history
-// Retrieved 2026-04-11, License - CC BY-SA 4.0
-foreach(var item in args)
-{
-//    Console.WriteLine(item.ToString());
-}
+            var dict = DictArgDestinations();  // class defined below
 
             for (int i = 0; i < args.Length; i++)
             {
-                if (!dict.TryGetValue(args[i], out var def)) // def is an ArgDef defined below
+                if (!dict.TryGetValue(args[i], out var def)) // def is an ArgSetter defined below
                     continue;   // i.e. just ignore it as a rogue parameter that does no harm
 
                 string? value = null;
 
                 if (def.RequiresValue)
                 {
-
                     if (i + 1 >= args.Length)
                         throw new ArgumentException($"Out of args. Missing value for {args[i]}");
                     if (dict.TryGetValue(args[i+1], out var def2))
-                        throw new ArgumentException($"Next arg recognised. Missing value for {args[i]}");
+                        throw new ArgumentException($"Next arg recognised as a flag (so it can't be a value). Missing value for {args[i]}");
 
                     value = args[++i];
                 }
 
-                def.Apply(config, value);  // Apply is the Action for args[i] from DictArgs below which mostly sets some Property in config
+                def.Apply(config, value);  // Apply is the Action for args[i] from DictArgDestinations below which mostly sets some Property in config
             }
 
             return config;
         }
 
-        private static Dictionary<string, ArgDef> DictArgs() =>
-            new(StringComparer.OrdinalIgnoreCase)
+        private static Dictionary<string, ArgSetter> DictArgDestinations() =>
+            new(StringComparer.OrdinalIgnoreCase) //   (StringComparer.OrdinalIgnoreCase) means that e.g. --HELP and --help are equivalent
             {
                 ["--help"] = new((c, v) => c.ShowHelp = true, false), // false here implies value not required i.e. flag only like debug
                 ["-h"]     = new((c, v) => c.ShowHelp = true, false),
-
-
                 ["--debug"] = new((c, v) => c.Debug = true, false),
 
                 ["--mp"]      = new((c, v) => c.MpFile = v, true),
                 ["--assets"]  = new((c, v) => c.AssetsFolder = v, true),
                 ["--mort"]    = new((c, v) => c.MortFile = v, true),
-                ["--agg"]     = new((c, v) => c.Agg = v, true), // target-typed new which is equivalent to new ArgDef((c, v) => c.Agg = v, true)
+                ["--agg"]     = new((c, v) => c.Agg = v, true), // target-typed new which is equivalent to new ArgSetter((c, v) => c.Agg = v, true)
                 ["--output"]  = new((c, v) => c.Output = v, true),
 
                 ["--age"]     = new((c, v) => c.Age = double.Parse(v), true),
@@ -62,13 +51,14 @@ foreach(var item in args)
                 ["--rate"]    = new((c, v) => c.Rate = double.Parse(v), true),
                 ["--years"]   = new((c, v) => c.Years = int.Parse(v), true),
             };
+
             
-        private class ArgDef
+        private class ArgSetter
         {
-            public Action<Config, string> Apply { get; }
+            public Action<Config, string> Apply { get; }  // no set i.e. it can only be set in the constructor (write-once)
             public bool RequiresValue { get; }
 
-            public ArgDef(Action<Config, string> apply, bool requiresValue)
+            public ArgSetter(Action<Config, string> apply, bool requiresValue)
             {
                 Apply = apply;
                 RequiresValue = requiresValue;
